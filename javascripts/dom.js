5ffd33c2615e6af94bfd1d9ea0fc95cc70071021
Alex Orga <alexorga.dev@gmail.com>
2018-05-28 19:23:28 -0500

    domString completed for all three options

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,69 +1,81 @@
 // const moment = require('moment');
 
-const outputDiv = $('#weather');
+const outputDivSingle = $('#weather');
+const outputDivMulti = $('#multiDay');
 
 const domString = (weatherToday) => {
   const iconCode = weatherToday.weather[0].icon;
   const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
   const pressureToIn = (weatherToday.main.pressure * 0.02953).toFixed(2);
   // const date = moment().format();
-  // let currentDay = '';
-  // currentDay = new Date(1527800400 * 1000).toString();
+  let currentDay = '';
+  currentDay = new Date(weatherToday.dt * 1000).toString();
   let domString = '';
+  domString +=  `<h1>${weatherToday.name}</h1>`;
   domString +=  `<div id="currentForecast">`;
   // domString +=    `<h5>${date}</h5>`;
-  domString +=    `<h1>${weatherToday.name}</h1>`;
+  domString +=    `<h2>${currentDay}</h2>`;
   domString +=    `<img id="domIcon" src="${iconUrl}">`;
-  domString +=    `<h3>${weatherToday.main.temp} degrees</h3>`;
+  domString +=    `<h3>${Math.round(weatherToday.main.temp)}&deg; F</h3>`;
   domString +=    `<h5>Conditions: ${weatherToday.weather[0].main}</h5>`;
   domString +=    `<h5>Air Pressure: ${pressureToIn} in.</h5>`;
   domString +=    `<h5>Wind Speed: ${weatherToday.wind.speed} mph</h5>`;
   domString +=  `</div>`;
-  printToDom(domString);
+  printToDomSingle(domString);
 };
 
 const domStringFiveDay = (weather) => {
   let domString = '';
-  domString += `<h1>${weather.city.name}</h1>`;
+  domString += `<h1>Next 5 Days</h1>`;
 
   for (let i = 4; i < 37; i += 8) {
+    let currentDay = '';
+    currentDay = new Date(weather.list[i].dt * 1000).toString();
     const iconCode = weather.list[i].weather[0].icon;
     const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
     const pressureToIn = (weather.list[i].main.pressure * 0.02953).toFixed(2);
 
     domString +=  `<div class="col-md-1 col-md-offset-1">`;
+    domString +=     `<h5>${currentDay}</h5>`;
     domString +=     `<img id="domIcon" src="${iconUrl}">`;
-    domString +=     `<h3>${weather.list[i].main.temp} degrees</h3>`;
+    domString +=     `<h3>${Math.round(weather.list[i].main.temp)}&deg; F</h3>`;
     domString +=     `<h5>Conditions: ${weather.list[i].weather[0].main}</h5>`;
     domString +=     `<h5>Air Pressure: ${pressureToIn} in.</h5>`;
     domString +=     `<h5>Wind Speed: ${weather.list[i].wind.speed} mph</h5>`;
     domString +=  `</div>`;
   }
-  printToDom(domString);
+  printToDomMulti(domString);
 };
 
 const domStringThreeDay = (weather) => {
   let domString = '';
-  domString += `<h1>${weather.city.name}</h1>`;
+  domString += `<h1>Next 3 Days</h1>`;
 
   for (let i = 4; i < 21; i += 8) {
+    let currentDay = '';
+    currentDay = new Date(weather.list[i].dt * 1000).toString();
     const iconCode = weather.list[i].weather[0].icon;
     const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
     const pressureToIn = (weather.list[i].main.pressure * 0.02953).toFixed(2);
 
     domString +=  `<div class="col-md-4">`;
+    domString +=     `<h5>${currentDay}</h5>`;
     domString +=     `<img id="domIcon" src="${iconUrl}">`;
-    domString +=     `<h3>${weather.list[i].main.temp} degrees</h3>`;
+    domString +=     `<h3>${Math.round(weather.list[i].main.temp)}&deg; F</h3>`;
     domString +=     `<h5>Conditions: ${weather.list[i].weather[0].main}</h5>`;
     domString +=     `<h5>Air Pressure: ${pressureToIn} in.</h5>`;
     domString +=     `<h5>Wind Speed: ${weather.list[i].wind.speed} mph</h5>`;
     domString +=  `</div>`;
   }
-  printToDom(domString);
+  printToDomMulti(domString);
 };
 
-const printToDom = (string) => {
-  outputDiv.html(string);
+const printToDomSingle = (string) => {
+  outputDivSingle.html(string);
+};
+
+const printToDomMulti = (string) => {
+  outputDivMulti.html(string);
 };
 
 module.exports = {
--- javascripts/events.js
+++ javascripts/events.js
@@ -1,22 +1,33 @@
 const openWeather = require('./openWeather');
 
+let inputValue = '';
 let zipCode = '';
 
 const enterKeyPress = () => {
   $(document).keypress((e) => {
     if (e.key === 'Enter') {
-      zipCode = $('#input').val();
-      openWeather.showSingleResults(zipCode);
-      forecastOptionEvents();
+      inputValue = $('#input').val();
+      if (inputValue.length === 5) {
+        zipCode = inputValue;
+        openWeather.showSingleResults(zipCode);
+        forecastOptionEvents();
+      } else {
+        alert('Please enter a valid 5 digit zip code');
+      }
     }
   });
 };
 
 const submitButton = () => {
   $('#submitBtn').click(() => {
-    zipCode = $('#input').val();
-    openWeather.showSingleResults(zipCode);
-    forecastOptionEvents();
+    inputValue = $('#input').val();
+    if (inputValue.length === 5) {
+      zipCode = inputValue;
+      openWeather.showSingleResults(zipCode);
+      forecastOptionEvents();
+    } else {
+      alert('Please enter a valid 5 digit zip code');
+    }
   });
 };
 
